6e0d185f0b1d3e51e22779b3613743911d7e27b3
Mateusz Kochelski <mateuszkochelski@Mateuszs-MacBook-Pro.local>
2025-03-28 21:42:20 +0100

    test is java spring boot works

---
+++ backend/src/main/java/agh/matury/MaturyApplication.java
@@ -0,0 +1,13 @@
+package agh.matury;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaturyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaturyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/agh/matury/MaturyApplicationTests.java
@@ -0,0 +1,13 @@
+package agh.matury;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaturyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
