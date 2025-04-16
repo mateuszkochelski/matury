b0cb751e2b51ca63e9a633187a7d95587d8d5d56
Mateusz Kochelski <mkochelski@student.agh.edu.pl>
2025-04-16 22:45:38 +0200

    db connection

---
+++ backend/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    id SERIAL NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password_hash VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
