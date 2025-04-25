f20490937caddd7787221e132e4f4cfd5e7d3d91
Mateusz Kochelski <127225346+mateuszkochelski@users.noreply.github.com>
2025-04-25 08:33:12 +0200

    db connection (#5)

--- backend/src/main/java/agh/matury/HelloController.java
+++ backend/src/main/java/agh/matury/HelloController.java
@@ -1,13 +1,21 @@
 package agh.matury;
 
+import agh.matury.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/")
-    public String hello() {
-        return "Hello, world!";
-    }
+  private final UserService userService;
+
+  public HelloController(UserService userService) {
+    this.userService = userService;
+  }
+
+  @GetMapping("/")
+  public String hello() {
+    userService.createUser("jeden", "dwa", "trzy");
+    return "Hello, world!";
+  }
 }
