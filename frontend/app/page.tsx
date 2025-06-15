36c268c3f412ba9b09e11c3d0bb1253bf33a7ca0
Konrad Armatys <karmatys888@gmail.com>
2025-06-15 13:59:37 +0200

    make design more responsive
    it works on 320px wide phones

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -37,10 +37,8 @@ export default async function Home() {
   const { number: pageNumber } = pageData;
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <CourseDegreeTable data={fields} />
-      </main>
-    </div>
+    <main className="items-center justify-items-center min-h-screen p-2 pb-20 gap-16 sm:p-8 md:p-16 lg:p-20 font-[family-name:var(--font-geist-sans)]">
+      <CourseDegreeTable data={fields} />
+    </main>
   );
 }
