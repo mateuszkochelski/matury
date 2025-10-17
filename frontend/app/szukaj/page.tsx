d76528a70d441ab5fbe34baa67039af31a735990
tozmuda <129078518+tozmuda@users.noreply.github.com>
2025-10-17 19:07:48 +0200

    create main page, moved search page

---
+++ frontend/app/szukaj/page.tsx
@@ -0,0 +1,29 @@
+import { BACKEND_URL } from "../constants";
+import { fetchData, TableSearchParams } from "@/components/custom-table/fetchData";
+import { MainTable } from "@/components/custom-table/main";
+import { FieldOfStudyData } from "@/components/custom-table/types";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams?: Promise<TableSearchParams>;
+}) {
+  const { pageSize, pageIndex, hiddenColumns } = (await searchParams) ?? {};
+
+  const response = await fetchData(`${BACKEND_URL}/api/field_of_study`, pageSize, pageIndex);
+  const data: FieldOfStudyData = await response.json();
+  const { content: fields, page: pageData } = data;
+  const { number: pageNumber, totalElements, size } = pageData;
+
+  return (
+    <main className="min-h-screen p-2 pb-20 sm:p-8 md:p-16 lg:p-20 font-[family-name:var(--font-geist-sans)]">
+      <MainTable
+        data={fields}
+        pageNumber={pageNumber}
+        pageSize={size}
+        totalElements={totalElements}
+        hiddenColumns={hiddenColumns?.split(",")}
+      />
+    </main>
+  );
+}
