2c2d057b6bf1811c5ad423936ae6fa4b6dbe420b
Konrad Armatys <karmatys888@gmail.com>
2025-12-15 22:37:36 +0100

    improve breeadcrumb

--- frontend/components/Breadcrumb.tsx
+++ frontend/components/Breadcrumb.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from "react";
 import { ChevronRight, Home } from "lucide-react";
 import Link from "next/link";
 
@@ -12,21 +13,36 @@ type BreadcrumbProps = {
 
 export function Breadcrumb({ items }: BreadcrumbProps) {
   return (
-    <nav className="flex items-center space-x-1 text-sm text-foreground/70 overflow-scroll [scrollbar-width:none]">
-      <Link href="/" className="flex items-center hover:text-foreground transition-colors">
-        <Home className="w-4 h-4" />
+    <nav className="flex items-center space-x-2 text-sm text-foreground/70 overflow-scroll [scrollbar-width:none]">
+      <Link
+        href="/"
+        className="flex items-center hover:text-foreground transition-colors"
+        title="Strona Główna"
+      >
+        <Home size={16} />
       </Link>
-      {items.map((item, index) => (
-        <div key={index} className="flex items-center space-x-1">
-          <ChevronRight className="w-4 h-4" />
+      <div>
+        <ChevronRight size={16} />
+      </div>
+      <Link href="/szukaj" className="hover:text-foreground transition-colors whitespace-nowrap">
+        Wyniki Wyszukiwania
+      </Link>
+      {items.map((item) => (
+        <Fragment key={`${item.name}${item.href}`}>
+          <div>
+            <ChevronRight size={16} />
+          </div>
           {item.href ? (
-            <Link href={item.href} className="hover:text-foreground transition-colors">
+            <Link
+              href={item.href}
+              className="hover:text-foreground transition-colors whitespace-nowrap"
+            >
               {item.name}
             </Link>
           ) : (
             <span className="text-foreground">{item.name}</span>
           )}
-        </div>
+        </Fragment>
       ))}
     </nav>
   );
