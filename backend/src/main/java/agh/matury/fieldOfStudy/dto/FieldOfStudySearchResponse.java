db600964a7b7dfb68f5d68b7275ed1a76789f433
mateuszkochelski <mkochelski@student.agh.edu.pl>
2025-12-28 18:43:54 +0100

    search name additional field made optional

--- backend/src/main/java/agh/matury/fieldOfStudy/dto/FieldOfStudySearchResponse.java
+++ backend/src/main/java/agh/matury/fieldOfStudy/dto/FieldOfStudySearchResponse.java
@@ -1,7 +1,10 @@
 package agh.matury.fieldOfStudy.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.util.List;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record FieldOfStudySearchResponse(
     List<FieldOfStudyExtendedDTO> content,
     PageMetadata page,
